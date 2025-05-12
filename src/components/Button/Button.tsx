1cf7fb25690f90e31224cc0bed8492dd31681226
gustavost26 <gteles@trivalor.pt>
2025-05-12 16:49:28 +0100

    feat: Button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,29 @@
+import {ActivityIndicator, TouchableOpacity} from 'react-native';
+import {Text} from '../Text/Text';
+import {useTheme} from '@shopify/restyle';
+import {Theme} from '../../theme/theme';
+import {Box} from '../Box/Box';
+
+interface ButtonProps {
+  title: string;
+  loading?: boolean;
+}
+
+export function Button({title, loading}: ButtonProps) {
+  return (
+    <Box
+      backgroundColor="buttonPrimary"
+      paddingHorizontal="s20"
+      height={50}
+      alignItems="center"
+      justifyContent="center">
+      {loading ? (
+        <ActivityIndicator />
+      ) : (
+        <Text preset="paragraphMedium" bold style={{color: 'white'}}>
+          {title}
+        </Text>
+      )}
+    </Box>
+  );
+}
