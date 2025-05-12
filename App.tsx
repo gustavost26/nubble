3d00cf307b2e151d7edd3b0b5b63df61f0edd421
gustavost26 <gteles@trivalor.pt>
2025-05-12 16:52:00 +0100

    feat: Home page and componentization test

---
+++ App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {SafeAreaView} from 'react-native';
+import {Text} from './src/components/Text/Text';
+import {ThemeProvider} from '@shopify/restyle';
+import {theme} from './src/theme/theme';
+import {Button} from './src/components/Button/Button';
+import {Box} from './src/components/Box/Box';
+
+function App(): React.JSX.Element {
+  return (
+    <ThemeProvider theme={theme}>
+      <SafeAreaView>
+        <Text preset="headingLarge" italic>
+          Test
+        </Text>
+        <Box marginBottom="s20">
+          <Button title="Entrar" />
+        </Box>
+        <Button loading title="Loding" />
+      </SafeAreaView>
+    </ThemeProvider>
+  );
+}
+
+export default App;
