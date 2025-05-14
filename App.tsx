dd2e716b0267fe233d942c756ff7951fb3453eeb
gustavost26 <gteles@trivalor.pt>
2025-05-14 13:01:11 +0100

    feat: New component Icon and image svg

--- App.tsx
+++ App.tsx
@@ -1,22 +1,39 @@
-import React from 'react';
-import {SafeAreaView} from 'react-native';
-import {Text} from './src/components/Text/Text';
 import {ThemeProvider} from '@shopify/restyle';
-import {theme} from './src/theme/theme';
-import {Button} from './src/components/Button/Button';
+import React from 'react';
+import {SafeAreaView, View} from 'react-native';
 
-function App(): React.JSX.Element {
+import {Box} from './src/components/Box/Box';
+import {Icon} from './src/components/Icon/Icon';
+
+import {Text} from './src/components/Text/Text';
+import {theme} from './src/theme/theme';
+
+function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView>
-        <Text preset="headingLarge" italic>
-          Test
-        </Text>
-        <Button marginBottom="s20" title="Primary" preset="primary" disabled />
-        <Button marginBottom="s20" title="Primary" preset="primary" />
-        <Button loading marginBottom="s20" title="Outline" preset="outline" />
-        <Button loading marginBottom="s20" title="Loding" disabled />
-        <Button loading title="Loding" />
+        <View style={{paddingHorizontal: 24}}>
+          <Text preset="headingLarge" italic>
+            Coffstack
+          </Text>
+
+          <Box flexDirection="row">
+            <Icon name="chevronRight" size={50} />
+            <Icon name="heartFill" color="buttonPrimary" />
+            <Icon name="profile" size={50} />
+            <Icon name="profileFill" size={50} />
+            <Icon name="heart" size={50} />
+            <Icon name="bellOn" color="carrotSecondary" size={50} />
+          </Box>
+          <Box flexDirection="row">
+            <Icon name="newPost" size={50} />
+            <Icon name="camera" size={50} />
+            <Icon name="chat" size={50} />
+            <Icon name="chatOn" color="error" size={50} />
+            <Icon name="flashOff" size={50} />
+            <Icon name="flashOn" size={50} />
+          </Box>
+        </View>
       </SafeAreaView>
     </ThemeProvider>
   );
