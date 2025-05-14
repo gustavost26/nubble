c7d6b4f0c4241c532ba62c85c92189dbc81d7cbd
gustavost26 <gteles@trivalor.pt>
2025-05-14 15:32:43 +0100

    chore: Login view

--- App.tsx
+++ App.tsx
@@ -3,36 +3,41 @@ import React from 'react';
 import {SafeAreaView, View} from 'react-native';
 
 import {Box} from './src/components/Box/Box';
-import {Icon} from './src/components/Icon/Icon';
 
 import {Text} from './src/components/Text/Text';
 import {theme} from './src/theme/theme';
+import {Button} from './src/components/Button/Button';
+import {TextInput} from './src/components/TextInput/TextInput';
+import {Icon} from './src/components/Icon/Icon';
 
 function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView>
         <View style={{paddingHorizontal: 24}}>
-          <Text preset="headingLarge" italic>
-            Coffstack
+          <Text mb="s8" preset="headingLarge">
+            Olá
           </Text>
-
-          <Box flexDirection="row">
-            <Icon name="chevronRight" size={50} />
-            <Icon name="heartFill" color="buttonPrimary" />
-            <Icon name="profile" size={50} />
-            <Icon name="profileFill" size={50} />
-            <Icon name="heart" size={50} />
-            <Icon name="bellOn" color="carrotSecondary" size={50} />
-          </Box>
-          <Box flexDirection="row">
-            <Icon name="newPost" size={50} />
-            <Icon name="camera" size={50} />
-            <Icon name="chat" size={50} />
-            <Icon name="chatOn" color="error" size={50} />
-            <Icon name="flashOff" size={50} />
-            <Icon name="flashOn" size={50} />
-          </Box>
+          <Text mb="s40" preset="paragraphLarge">
+            Digite seu e-mail e senha para entrar
+          </Text>
+          <TextInput
+            errorMessage="Mensagem de error"
+            label="E-mail"
+            placeholder="Digite seu e-mail"
+            boxProps={{mb: 's20'}}
+          />
+          <TextInput
+            label="Senha"
+            placeholder="Digite sua senha"
+            RightComponent={<Icon color="gray2" name="eyeOn" />}
+            boxProps={{mb: 's10'}}
+          />
+          <Text color="primary" preset="paragrapSmall" bold>
+            Esqueci minha senha
+          </Text>
+          <Button mt="s48" title="Entrar" />
+          <Button mt="s12" title="Criar uma conta" preset="outline" />
         </View>
       </SafeAreaView>
     </ThemeProvider>
