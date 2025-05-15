496480a5a45e7e2d0160f0aaa1e7af9eb93a759c
gustavost26 <gteles@trivalor.pt>
2025-05-15 14:01:42 +0100

    feat: New component input password

---
+++ src/components/PasswordInput/PasswordInput.tsx
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+import {TextInput, TextInputProps} from '../TextInput/TextInput';
+import {Icon} from '../Icon/Icon';
+
+type PasswordInpuntProps = Omit<TextInputProps, 'RightComponent'>;
+
+export function PasswordInput(props: PasswordInpuntProps) {
+  const [isSecureTextEntry, setIsSecureTextEntry] = useState(true);
+
+  function toggleSecureTextEntry() {
+    setIsSecureTextEntry(prev => !prev);
+  }
+
+  return (
+    <TextInput
+      secureTextEntry={isSecureTextEntry}
+      {...props}
+      RightComponent={
+        <Icon
+          onPress={toggleSecureTextEntry}
+          color="gray2"
+          name={isSecureTextEntry ? 'eyeOn' : 'eyeOff'}
+        />
+      }
+    />
+  );
+}
