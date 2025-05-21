f1fa2596c86c163aa5a929f51af97acd7e630235
gustavost26 <gteles@trivalor.pt>
2025-05-21 20:49:28 +0100

    feat: new optimized form components

---
+++ src/components/Form/FormPasswordInput.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import {Controller, FieldValues, UseControllerProps} from 'react-hook-form';
+import {
+  PasswordInpuntProps,
+  PasswordInput,
+} from '../PasswordInput/PasswordInput';
+
+export function FormPasswordInput<formType extends FieldValues>({
+  control,
+  name,
+  rules,
+  ...PasswordInputProps
+}: PasswordInpuntProps & UseControllerProps<formType>) {
+  return (
+    <Controller
+      control={control}
+      name={name}
+      rules={rules}
+      render={({field, fieldState}) => (
+        <PasswordInput
+          errorMessage={fieldState.error?.message}
+          value={field.value}
+          onChangeText={field.onChange}
+          {...PasswordInputProps}
+        />
+      )}
+    />
+  );
+}
