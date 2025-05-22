1aef1580c1cbca6c1a7207752714e87cfdf0f90f
gustavost26 <gteles@trivalor.pt>
2025-05-23 00:11:50 +0100

    feat: Validation scheme with zod.

--- src/screens/auth/SignUpScreen/signUpSchema.ts
+++ src/screens/auth/SignUpScreen/signUpSchema.ts
@@ -3,9 +3,11 @@ import { z } from 'zod';
 const userNameRegex = /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/gim;
 
 export const sigUpSchema = z.object({
-  username: z.string().regex(userNameRegex),
-  fullName: z.string().min(3, 'Nome muito curto').max(50, 'Nome muito longo'),
-  email: z.string().email(),
+  username: z.string().regex(userNameRegex).toLowerCase(),
+  fullName: z.string().min(3, 'Nome muito curto').max(50, 'Nome muito longo').transform(value => {
+    return value.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join(' ');
+  }),
+  email: z.string().email('E-mail inválido'),
   password: z.string().min(8, 'Senha muito curta'),
 });
 
