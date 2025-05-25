2189b204892010d4244249dbbdbd7d44d7413b6a
gustavost26 <gteles@trivalor.pt>
2025-05-25 12:28:27 +0100

    chore: Import configuration

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    ['module-resolver', {
+      root: '.',
+      alias: {
+        '@components': './src/components',
+        '@hooks': './src/hooks',
+        '@routes': './src/routes',
+        '@screens': './src/screens',
+        '@theme': './src/theme',
+        '@utils': './src/utils',
+      }
+    }]
+  ]
 };
