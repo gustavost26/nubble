9f7c1b921cb13e303183146d399068f707a27123
gustavost26 <gteles@trivalor.pt>
2025-06-19 11:24:06 +0100

    feat: New Screen

---
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import {Button, Screen, Text} from '@components';
+import {AppScreenProps} from '@routes';
+
+//type ScreenProps = NativeStackScreenProps<AppStackParamList, 'HomeScreen'>;
+
+export function HomeScreen({navigation}: AppScreenProps<'HomeScreen'>) {
+  return (
+    <Screen>
+      <Text preset="headingSmall">Home Screen</Text>
+      <Button
+        title="Settings"
+        onPress={() => navigation.navigate('SettingsScreen')}
+      />
+    </Screen>
+  );
+}
--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,3 +1,11 @@
+//App
+export * from './app/HomeScreen/HomeScreen';
+export * from './app/SettingsScreen/SettingsScreen';
+export * from './app/FavoriteScreen/FavoriteScreen';
+export * from './app/MyProfileScreen/MyProfileScreen';
+export * from './app/NewPostScreen/NewPostScreen';
+
+//Auth
 export * from './auth/ForgotPasswordScreen/ForgotPasswordScreen';
 export * from './auth/LoginScreen/LoginScreen';
 export * from './auth/SignUpScreen/SignUpScreen';
