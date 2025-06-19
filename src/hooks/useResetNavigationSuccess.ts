d251c22069dc240c2ce3496bc498d51328fa2f5a
gustavost26 <gteles@trivalor.pt>
2025-06-19 11:22:55 +0100

    feat: Tab Navigation

--- src/hooks/useResetNavigationSuccess.ts
+++ src/hooks/useResetNavigationSuccess.ts
@@ -1,11 +1,11 @@
-import { useNavigation } from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
 
-import { RootStackParamList } from '@routes';
+import {AuthStackParamList} from '@routes';
 
 export function useResetNavigationSuccess() {
   const navigation = useNavigation();
 
-  function reset(params: RootStackParamList['SuccessScreen']) {
+  function reset(params: AuthStackParamList['SuccessScreen']) {
     navigation.reset({
       index: 1,
       routes: [
@@ -20,5 +20,5 @@ export function useResetNavigationSuccess() {
     });
   }
 
-  return { reset };
+  return {reset};
 }
